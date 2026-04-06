d51c91f2ea4a1c345b9e2758ebf5abaee1e3c3a5
Ishan662 <ishanmadurangai662@gmail.com>
2026-04-06 10:53:08 +0530

    feat: enhance user validation and update dependencies

--- user-mgmt-system/gateway/main.go
+++ user-mgmt-system/gateway/main.go
@@ -12,10 +12,21 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/ishan662/user-service/pkg/userclient"
 	"github.com/nats-io/nats.go"
+	"github.com/go-playground/validator/v10"
 )
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+var validate = validator.New()
+
+func formatValidationError(err error) string {
+	var sb strings.Builder
+	for _, err := range err.(validator.ValidationErrors) {
+		sb.WriteString(err.Field() + " is " + err.Tag() + "; ")
+	}
+	return sb.String()
 }
 
 func handleCorrections(client *userclient.Client) http.HandlerFunc {
@@ -84,16 +95,22 @@ func main() {
 
 	r.Post("/users", func(w http.ResponseWriter, r *http.Request) {
 		var userReq struct {
-			FirstName string `json:"first_name"`
-			LastName  string `json:"last_name"`
-			Email     string `json:"email"`
-			Phone     string `json:"phone"`
-			Age       int32  `json:"age"`
+			FirstName string `json:"first_name" validate:"required, min=2,max=50"`
+			LastName  string `json:"last_name" validate:"required, min=2,max=50"`
+			Email     string `json:"email" validate:"required,email"`
+			Phone     string `json:"phone" validate:"omitempty,e164"`
+			Age       int32  `json:"age" validate:"omitempty, gte=0, lte=150"`
 		}
 
 		if err := json.NewDecoder(r.Body).Decode(&userReq); err != nil {
 			slog.Error("Failed to decode request ", "error", err)
 			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+
+		if err := validate.Struct(userReq); err != nil {
+			slog.Warn("validation failed", "error", err)
+			http.Error(w, "Validation Error: "+formatValidationError(err), http.StatusUnprocessableEntity)
 			return
 		}
 
